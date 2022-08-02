f504131c5cc3b66faf342220ebd80e036063027e
Elder Kampbell <elderkampbell@hotmail.com>
2022-08-02 11:50:58 -0300

    Requisitos 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const total = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach((element) => {
+    if (element.age < 18) {
+      total.child += 1;
+    } else if (element.age >= 18 && element.age < 50) {
+      total.adult += 1;
+    } else {
+      total.senior += 1;
+    }
+  });
+  return total;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) { return 0; }
+  const obj = countEntrants(entrants);
+  return (obj.child * prices.child) + (obj.adult * prices.adult) + (obj.senior * prices.senior);
 }
 
 module.exports = { calculateEntry, countEntrants };
