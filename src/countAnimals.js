aada6524b77a17cf0c80c4e7554a0ff09dadc2b6
Elder Kampbell <elderkampbell@hotmail.com>
2022-08-01 23:54:31 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce((acc, { name, residents }) => {
+      acc[name] = residents.length;
+      return acc;
+    }, {});
+  }
+  if (animal.sex === undefined) {
+    return species.find((specie) => (specie.name === animal.specie)).residents.length;
+  }
+  if (animal.sex !== undefined) {
+    return species.find((specie) => specie.name === animal.specie).residents
+      .filter((filtered) => filtered.sex === animal.sex).length;
+  }
 }
-
 module.exports = countAnimals;
