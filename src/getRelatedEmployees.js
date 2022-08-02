306e94f80b3d6ecf0f056cf830cd23d2b773d5cb
Elder Kampbell <elderkampbell@hotmail.com>
2022-08-01 22:13:38 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((manager) => manager.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const empregados = employees.filter((managed) => managed.managers.includes(managerId));
+    return empregados.reduce((acc, curr) => acc.concat(`${curr.firstName} ${curr.lastName}`), []);
+  }
+
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
