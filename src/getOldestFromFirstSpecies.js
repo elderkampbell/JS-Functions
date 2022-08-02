6037d971d79b3ff5d10815d5d6417b33efd95e0f
Elder Kampbell <elderkampbell@hotmail.com>
2022-08-02 16:14:26 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const responsavel = employees.find((employee) => employee.id === id).responsibleFor;
+  const animal = species.find((specie) => responsavel[0] === specie.id).residents;
+  const comparador = animal
+    .reduce((oldest, animals) => (oldest.age < animals.age ? animals : oldest));
+  return Object.values(comparador);
 }
+
+// console.log(getOldestFromFirstSpecies('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = getOldestFromFirstSpecies;
